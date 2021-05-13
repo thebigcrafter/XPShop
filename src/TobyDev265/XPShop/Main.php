0aea35f079e1a101565563a07fe46fed99f5ec56
MintoD <mintothedev@protonmail.com>
2021-05-13 20:42:48 +0700

    Revert "Update v1.0.5"
    This reverts commit f516de6ec9d0b7df72d02cfd9ad765a655428aa2.

--- src/TobyDev265/XPShop/Main.php
+++ src/TobyDev265/XPShop/Main.php
@@ -30,7 +30,7 @@ class Main extends PluginBase
         @mkdir($this->getDataFolder());
         $this->saveDefaultConfig();
         $this->cfg = new Config($this->getDataFolder() . "config.yml", Config::YAML);
-        UpdateNotifier::checkUpdate("XPShop", "1.0.5");
+        UpdateNotifier::checkUpdate("XPShop", "1.0.4");
     }
 
     /**
@@ -82,7 +82,7 @@ class Main extends PluginBase
             $player->sendMessage($this->cfg->get("sellSuccess"));
         });
         $form->setTitle($this->cfg->get("sell_title"));
-        $form->addSlider($this->cfg->get("sell_slider_label"), 1, $player->getXpLevel());
+        $form->addSlider($this->cfg->get("slider_label"), 1, $player->getXpLevel());
         $player->sendForm($form);
         return true;
     }
@@ -93,13 +93,21 @@ class Main extends PluginBase
             if ($result === null) {
                 return true;
             }
-            $money = $result[0] * $this->cfg->get("xpPriceWhenBuy");
-            $player->setXpLevel($player->getXpLevel() + $result[0]);
-            EconomyAPI::getInstance()->reduceMoney($player, $money);
-            $player->sendMessage($this->cfg->get("buySuccess"));
+            if (!is_numeric($result[0])) {
+                $player->sendMessage(C::RED . $this->cfg->get("isNotANumber"));
+            } else {
+                $money = $result[0] * $this->cfg->get("xpPriceWhenBuy");
+                if (EconomyAPI::getInstance()->myMoney($player) >= $money) {
+                    EconomyAPI::getInstance()->reduceMoney($player, $money);
+                    $player->sendMessage($this->cfg->get("buySuccess"));
+                    $player->setXpLevel($player->getXpLevel() + $result[0]);
+                } else {
+                    $player->sendMessage(C::RED . $this->cfg->get("doNotEnoughMoney"));
+                }
+            }
         });
         $form->setTitle($this->cfg->get("buy_title"));
-        $form->addSlider($this->cfg->get("buy_slider_label"), 1, floor(EconomyAPI::getInstance()->myMoney($player) / $this->cfg->get("xpPriceWhenBuy")) - 1);
+        $form->addInput($this->cfg->get("input_label"));
         $player->sendForm($form);
         return true;
     }
